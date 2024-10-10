d4f75cb275d61b840fd286dd23654b93decd7dd4
qawsedrftgzh <59800573+qawsedrftgzh@users.noreply.github.com>
2024-10-10 12:02:45 +0200

    new changes

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   #programs.hyprland.enable = true;
@@ -62,7 +62,7 @@
   #services.xserver.desktopManager.plasma6.enable = true;
   #programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass";
   services.xserver.displayManager.sddm = {
-    enable = true;
+    enable = false;
     theme = "catppuccin-mocha";
     package = pkgs.kdePackages.sddm;
   };
